a6ab8d268bc1698172711bc6f48aeabea2ef7d12
thatknitter <kblewis.flt@gmail.com>
2014-10-22 14:58:47 -0500

    built index page. Got JSONP called correctly.

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,22 @@
-function getJSON(url, cb){	
-	var xhr = new XMLHttpRequest();
-	xhr.open('GET', url);
-	xhr.onload = function(){
-		cb(JSON.parse(xhr.responseText));		
-	};
-	xhr.send();
+var url = 'http://api.wunderground.com/api/b87f952cedc29268/forecast10day/q/CA/San_Francisco.json';
+
+//function getJSON(url, cb){	
+//	var xhr = new XMLHttpRequest();
+//	xhr.open('GET', url);
+//	xhr.onload = function(){
+//		cb(JSON.parse(xhr.responseText));		
+//	};
+//	xhr.send();
+//}
+
+function getJSONP(url, cbName) {
+   var $script = document.createElement('script');
+   $script.src = url + '?callback=' + cbName;
+   document.body.appendChild($script);
 }
 
+function myAwesomeFunction(data){
+        console.log(data);
+};
 
+getJSONP(url, 'myAwesomeFunction');
