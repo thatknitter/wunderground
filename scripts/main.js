7adf9e898a7963e2d8eb881870bcf0476f742e55
thatknitter <kblewis.flt@gmail.com>
2014-10-23 10:56:59 -0500

    IT WORKS!!!!!

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,45 @@
 var url = 'http://api.wunderground.com/api/b87f952cedc29268/forecast10day/q/CA/San_Francisco.json';
 
-//function getJSON(url, cb){	
-//	var xhr = new XMLHttpRequest();
-//	xhr.open('GET', url);
-//	xhr.onload = function(){
-//		cb(JSON.parse(xhr.responseText));		
-//	};
-//	xhr.send();
-//}
-
 function getJSONP(url, cbName) {
    var $script = document.createElement('script');
    $script.src = url + '?callback=' + cbName;
    document.body.appendChild($script);
 }
 
-function myAwesomeFunction(data){
+function arrayGrab(data){
         console.log(data);
-};
+        var data = data.forecast.simpleforecast.forecastday;
+        var $target = document.querySelector('ul');
+        for (var i = 0; i < 6; i++){
+            addItemToList($target, data[i]);
 
-getJSONP(url, 'myAwesomeFunction');
+         }
+}
+
+
+function addItemToList($list, items){
+  // var $list = arguments[0]
+  // var items = arguments[1]
+        
+ var $li = document.createElement('li');
+ var $icon = document.createElement('img');
+ var $weekday = document.createElement('h3');
+ var $high = document.createElement('p');
+ var $low = document.createElement('p');
+
+    $weekday.innerHTML = items.date.weekday;
+    $high.innerHTML = items.high.fahrenheit;
+    $low.innerHTML = items.low.fahrenheit;
+    $icon.src = items.icon_url;
+    $li.appendChild($weekday);
+    $li.appendChild($icon);
+    $li.appendChild($high);
+    $li.appendChild($low);
+    $list.appendChild($li);
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  
+  getJSONP(url, 'arrayGrab');
+  
+});
